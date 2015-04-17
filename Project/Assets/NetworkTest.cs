ed0411e3e5d9490a2e0073cc9d6536a92edabacc
Zack Maldonado <stjohnzack@yahoo.com>
2015-04-16 20:07:03 -0400

    initing project

---
+++ Project/Assets/NetworkTest.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NetworkTest : MonoBehaviour {
+    public Text textOutput;
+
+	// Use this for initialization
+    IEnumerator Start()
+    {
+        WWW www = new WWW("www.pawz.ninja/pets");
+        yield return www;
+        textOutput.text = www.text;
+	}
+
+    void ConnectToServer()
+    {
+        //NetworkConnectionError conn = Network.Connect("www.google.com", 80, "");
+        //Debug.Log(conn);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

240c700ca743ffbff80fa64e3cb3707eef1e82e0
Zack Maldonado <stjohnzack@yahoo.com>
2015-04-17 12:34:32 -0400

    Adding JSON.NET. Sending json successfully to server

---
+++ Project/Assets/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets
+{
+    class Player
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string x { get; set; }
+        public string y { get; set; }
+    }
+}
--- Project/Assets/PlayerScript.cs
+++ Project/Assets/PlayerScript.cs
@@ -1,31 +1,53 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine.UI;
+using Newtonsoft.Json;
+using Assets;
 
 public class PlayerScript : MonoBehaviour {
     public Text textOutput;
     public bool IsUser = false;
-    public string url;
-    public int port;
-    public string route;
+    public string url = "52.6.61.57/user";
 
     // Use this for initialization
     void Start()
     {
         string deviceID = SystemInfo.deviceUniqueIdentifier;
+        
+        /*
         WWWForm form = new WWWForm();
         form.AddField("id", deviceID);
         form.AddField("name", deviceID);
         form.AddField("x", this.transform.position.x.ToString());
         form.AddField("y", this.transform.position.y.ToString());
+        */
 
-        WWW www = new WWW(url + ":" + port + route, form);
+        Player player = new Player();
+        player.id = deviceID;
+        player.name = deviceID;
+        player.x = this.transform.position.x.ToString();
+        player.y = this.transform.position.y.ToString();
+
+        string jsonString = JsonConvert.SerializeObject(player);
+        Debug.Log(jsonString);
+
+        //Hashtable headers = form.headers;
+        Hashtable headers = new Hashtable();
+        headers.Add("Content-Type", "application/json");
+        //headers.Add("Content-Type", "application/json");
+
+        byte[] rawData = System.Text.Encoding.ASCII.GetBytes(jsonString);
+        //Debug.Log(System.Text.Encoding.Default.GetString(rawData));
+
+        WWW www = new WWW(url, rawData, headers);
         StartCoroutine(WaitForRequest(www));
     }
 
     // Update is called once per frame
     void Update()
     {
+        /*
         string deviceID = SystemInfo.deviceUniqueIdentifier;
         WWWForm form = new WWWForm();
         form.AddField("id", deviceID);
@@ -34,7 +56,7 @@ public class PlayerScript : MonoBehaviour {
         form.AddField("y", this.transform.position.y.ToString());
 
         WWW www = new WWW(url + ":" + port + route, form);
-        StartCoroutine(WaitForRequest(www));
+        StartCoroutine(WaitForRequest(www));*/
     }
 
     IEnumerator WaitForRequest(WWW www)
